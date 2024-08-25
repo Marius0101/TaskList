e0b23126faef46afd927ee3e6f516559d61d65df
Marius0101 <Marius_Mihalcea@waters.com>
2024-08-25 10:18:52 +0300

    add header + input

--- task-list/src/App.tsx
+++ task-list/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import InputFeild from './components/InputFeild';
 
-function App() {
+const App:  React.FC = () => {
   return (
-    <div className='App'>Hello World</div>
+    <div className='App'>
+      <span className='heading'>Task List</span>
+      <InputFeild />
+    </div>
   );
 }
 
