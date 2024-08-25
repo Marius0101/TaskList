c921ce2501c48c0201028cbe94bdee9890e163f6
Marius0101 <Marius_Mihalcea@waters.com>
2024-08-25 12:44:30 +0300

    implement done and delete  task

--- task-list/src/components/ToDoItem.tsx
+++ task-list/src/components/ToDoItem.tsx
@@ -3,6 +3,7 @@ import { ToDo } from '../model'
 import { CiEdit } from "react-icons/ci";
 import { MdOutlineDone,MdDelete  } from "react-icons/md";
 import "./styles.css"
+import ToDoList from './ToDoList';
 
 interface Props{
   toDo: ToDo
@@ -11,20 +12,49 @@ interface Props{
 
 }
 const ToDoItem: React.FC<Props> = ({toDo, ListToDo, setListToDo}) => {
+  
+  const handleDone = (id: number) =>{
+    setListToDo( ListToDo.map((task) => 
+      task.id === id ? {...task, isCompleted: !task.isCompleted } : task
+    ))
+  };
+  const handleDelet = (id: number) =>{
+    setListToDo(
+      ListToDo.filter( task => task.id !== id)
+    )
+  };
+
   return (
     <form className='toDoItem'>
-      <span className='toDoItem-text'>
-        {toDo.task}
-      </span>
+      {
+        toDo.isCompleted ? (
+          <s className='toDoItem-text'>
+            {toDo.task}
+          </s>
+        ):
+        (
+          <span className='toDoItem-text'>
+            {toDo.task}
+          </span>
+        )
+      }
       <div>
         <span className='icon'>
-        <MdOutlineDone />
+        <MdOutlineDone 
+          onClick={
+            () => handleDone(toDo.id)
+          }
+        />
         </span>
         <span className='icon'>
           <CiEdit />
         </span>
         <span className='icon'>
-          <MdDelete />
+          <MdDelete 
+            onClick={
+              () => handleDelet(toDo.id)
+            }
+          />
         </span>
       </div>
       
